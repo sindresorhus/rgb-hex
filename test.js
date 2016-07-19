dc8d637b3fef9528855a5a73f6a583cae743556a
Gibran Malheiros <gibatronic@gmail.com>
2016-07-19 12:41:14 -0300

    upgraded ava

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 'use strict';
+
 var test = require('ava');
 var rgbHex = require('./');
 
 test(function (t) {
-	t.assert(rgbHex(0, 0, 0) === '000000');
-	t.assert(rgbHex(65, 131, 196) === '4183c4');
+	t.is(rgbHex(0, 0, 0), '000000');
+	t.is(rgbHex(65, 131, 196), '4183c4');
 });
